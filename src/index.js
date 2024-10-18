932b884a4d2e4cfe8c283c70e586fd07fa628402
Shumaila <sayedshumaila27@gmail.com>
2024-10-19 01:59:17 +0530

    stye add task dialog box

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css";
 console.log(1)
+
+document.querySelector('.add-task').addEventListener('click', () => {
+    const dialog = document.querySelector('dialog');
+    dialog.show();
+});
+
+document.getElementById('close-dialog').addEventListener('click', () => {
+    const dialog = document.querySelector('dialog');
+    dialog.close(); 
+});
