7575200922cc0ace2d72e6138104c84f61f0ec3c
Shumaila <sayedshumaila27@gmail.com>
2024-10-23 00:30:17 +0530

    final UI changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import "./styles.css";
 console.log(1)
 
 document.querySelector('.add-task').addEventListener('click', () => {
-    const dialog = document.querySelector('dialog');
-    dialog.show();
+    const dialog = document.querySelector('.side-dialog');
+    dialog.show();    
 });
 
 document.getElementById('close-dialog').addEventListener('click', () => {
-    const dialog = document.querySelector('dialog');
+    const dialog = document.querySelector('.side-dialog');
     dialog.close(); 
 });
+
+document.querySelector('.add-icon').addEventListener('click', () => {
+    const projectDialog = document.querySelector('.Project-dialog');
+    projectDialog.show();
+})
+
+document.querySelector('.project-cancel').addEventListener('click', () => {
+    const projectDialog = document.querySelector('.Project-dialog');
+    projectDialog.close();
+})
+ 
