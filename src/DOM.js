2f054084c85c3365e0f7f525bed2d1642fa774e4
Shumaila <sayedshumaila27@gmail.com>
2024-10-27 15:23:05 +0530

    update project and options based on input

--- src/DOM.js
+++ src/DOM.js
@@ -1,8 +1,5 @@
 import addicon from './assets/add_28dp_F6EBFC_FILL0_wght400_GRAD0_opsz24.svg';
 import cancelIcon from './assets/cancel_26dp_F02424_FILL0_wght400_GRAD0_opsz24.svg';
-import editicon from './assets/edit_28dp_F6EBFC_FILL0_wght400_GRAD0_opsz24.svg';
-import deleteicon from './assets/delete_28dp_F6EBFC_FILL0_wght400_GRAD0_opsz24.svg';
-
 
 function loadTaskBoard() {
     console.log('TaskBoard check')
@@ -25,13 +22,10 @@ function loadTaskBoard() {
     addTask.textContent = 'Add Tasks';
     secondDiv.appendChild(addTask);
 
-
     const sideDialog = document.createElement('dialog');
     sideDialog.className = 'side-dialog';
-
     const formDiv = document.createElement('div');
     formDiv.className = 'div-form';
-
     const sideForm = document.createElement('form');
     sideForm.className = 'side-form';
 
@@ -71,29 +65,14 @@ function loadTaskBoard() {
         } else if (n == 3) {
             priorityOptions.textContent = 'Urgent';
         }
-        taskPriority.appendChild(priorityOptions);
-    }
+       taskPriority.appendChild(priorityOptions);
+     }
 
     sideForm.appendChild(taskPriority);
 
     const taskProject = document.createElement('select');
     taskProject.id = 'task-projects';
     taskProject.name = 'project';
-
-    for(let n = 0; n < 4; n++){
-        const projectOptions = document.createElement('option');
-        if(n == 0){
-            projectOptions.textContent = 'Tasks';
-            projectOptions.setAttribute('selected', true);
-        } else if (n == 1) {
-            projectOptions.textContent = 'Chores';
-        } else if (n == 2) {
-            projectOptions.textContent = 'Mental health';
-        } else if (n == 3) {
-            projectOptions.textContent = 'Japan Trip';
-        }
-        taskProject.appendChild(projectOptions);
-    }
     sideForm.appendChild(taskProject);
 
     const buttonDiv = document.createElement('div');
@@ -111,7 +90,6 @@ function loadTaskBoard() {
     buttonDiv.appendChild(addBtn);
 
     sideForm.appendChild(buttonDiv);
-
     formDiv.appendChild(sideForm);
     sideDialog.appendChild(formDiv);
     secondDiv.appendChild(sideDialog);
@@ -134,15 +112,12 @@ function loadTaskBoard() {
     addIconDiv.appendChild(addIcon);
 
     projects.appendChild(addIconDiv)
-
     secondDiv.appendChild(projects);
 
     const projectDialog = document.createElement('dialog');
     projectDialog.className = 'Project-dialog';
-
     const projectForm = document.createElement('form');
     projectForm.method = 'dialog';
-
     const projectInputDiv = document.createElement('div');
     projectInputDiv.className = "project-name-input";
 
@@ -154,6 +129,7 @@ function loadTaskBoard() {
 
     const projectInput = document.createElement('input');
     projectInput.setAttribute("type", "text");
+    projectInput.id = 'project-Name'
     projectInput.placeholder = "Project Name + [Enter]";
     projectInput.required = true;
     projectInputDiv.appendChild(projectInput);
@@ -164,40 +140,25 @@ function loadTaskBoard() {
 
     const projectList = document.createElement('div');
     projectList.className = 'project-list';
-
-    for(let i = 0; i < 3; i++){
-        const project = document.createElement('div');
-        project.className = 'project';
-
-        if(i == 0){
-            project.textContent = 'Chores';
-        } else if (i == 1) {
-            project.textContent = 'Mental health';
-        } else if (i == 2) {
-            project.textContent = 'Japan Trip';
-      }
-
-      const projectIcon = document.createElement('div');
-      projectIcon.className = 'project-icon';
-
-      const editIcon = document.createElement('img');
-      editIcon.src = editicon;
-      editIcon.title = 'Edit';
-      projectIcon.appendChild(editIcon);
-
-      const deleteIcon = document.createElement('img');
-      deleteIcon.src = deleteicon;
-      deleteIcon.title = 'Delete';
-      projectIcon.appendChild(deleteIcon);
-
-      project.appendChild(projectIcon);
-      projectList.appendChild(project);
-    }
-
     secondDiv.appendChild(projectList);
+
     sideBar.appendChild(secondDiv);
 
+    const mainContent = document.querySelector('.main-content');
+    const mainTab = document.createElement('div');
+    mainTab.className = 'main-tab';
 
+    const heading = document.createElement('h2');
+    heading.textContent = 'All Tasks';
+    mainTab.appendChild(heading);
+
+    mainContent.appendChild(mainTab);
+
+    const taskList = document.createElement('div');
+    taskList.className = 'task-list';
+    mainContent.appendChild(taskList);
+
+ // Event Listeners
     document.querySelector('.add-task').addEventListener('click', () => {
         const dialog = document.querySelector('.side-dialog');
         dialog.show();    
@@ -218,89 +179,6 @@ function loadTaskBoard() {
         const projectDialog = document.querySelector('.Project-dialog');
         projectDialog.close();
     })
-
-    const mainContent = document.querySelector('.main-content');
-
-    const mainTab = document.createElement('div');
-    mainTab.className = 'main-tab';
-
-    const heading = document.createElement('h2');
-    heading.textContent = 'All Tasks';
-    mainTab.appendChild(heading);
-
-    mainContent.appendChild(mainTab);
-
-    const taskList = document.createElement('div');
-    taskList.className = 'task-list';
-
-    for( let i = 0; i < 3; i++) {
-        const task = document.createElement('div');
-        task.className = 'task';
-
-        const taskContent = document.createElement('div');
-        taskContent.className = 'task-content'
-
-        const roundMark = document.createElement('span');
-        roundMark.className = 'round-mark';
-        taskContent.appendChild(roundMark);
-
-        const title = document.createElement('p');
-        title.className = 'title';
-
-        const description = document.createElement('p');
-        description.className = 'description';
-
-        if(i == 0){
-            title.textContent = 'CleanUp';
-            description.textContent = 'Deep clean bedroom, living room, bathroom, and kitchen.';
-        } else if (i == 1) {
-            title.textContent = 'Finish Home Page';
-            description.textContent = 'Fix bugs';
-        } else if (i == 2) {
-            title.textContent = 'Plan itinerary';
-            description.textContent = 'Search for beautifull places.';
-        }
-        taskContent.appendChild(title);
-        taskContent.appendChild(description);
-
-        task.appendChild(taskContent);
-
-        const taskDetails = document.createElement('div');
-        taskDetails.className = "task-details";
-
-        const priority = document.createElement('p');
-        priority.className = 'priority'
-        const date = document.createElement('p');
-        date.className = 'date';
-        const projectTitle = document.createElement('p');
-        projectTitle.className = 'project-title';
-
-        if(i == 0){
-            priority.textContent = 'low';
-            priority.classList.add('low');
-            date.textContent = '5 Oct 2024';
-            projectTitle.textContent = '#Chores';
-        } else if (i == 1) {
-            priority.textContent = 'Urgent';
-            priority.classList.add('urgent');
-            date.textContent = '4 Oct 2024';
-            projectTitle.textContent = '#Website';
-        } else if (i == 2) {
-            priority.textContent = 'Important';
-            priority.classList.add('important');
-            date.textContent = '8 Oct 2024';
-            projectTitle.textContent = '#Japan Trip';
-        }
-
-        taskDetails.appendChild(priority);
-        taskDetails.appendChild(date);
-        taskDetails.appendChild(projectTitle);
-
-        task.appendChild(taskDetails);
-        taskList.appendChild(task);
-    }
-
-    mainContent.appendChild(taskList);
 }
 
 export { loadTaskBoard }
--- src/task.js
+++ src/task.js
@@ -1,24 +1,23 @@
-import { loadTaskBoard } from  "./DOM";
+  import { loadTaskBoard } from  "./DOM";
+  import deleteicon from './assets/delete_28dp_F6EBFC_FILL0_wght400_GRAD0_opsz24.svg';
   loadTaskBoard();
 
-class Task {
-  constructor(name, description, date, priority, project) {
-    this.name = name;
-    this.description = description;
-    this.date = date;
-    this.priority = priority;
-    this.project = project;
-  }
-
-  completeTask() {
-    allTask.splice(allTask.indexOf(this), 1);
-  }
-}
+  class Task {
+    constructor(name, description, date, priority, project) {
+     this.name = name;
+     this.description = description;
+     this.date = date;
+     this.priority = priority;
+     this.project = project;
+    }
+   completeTask() {
+     allTask.splice(allTask.indexOf(this), 1);
+   }
+ }
 
   let allTask = [];
   const form = document.querySelector(".side-dialog");
   let taskList = document.querySelector('.task-list');
-  // let submitTask = document.querySelector('#addToTask';)
 
   form.addEventListener('submit', (e)=> {
     e.preventDefault();
@@ -32,76 +31,162 @@ class Task {
     addTaskToAllTasks(taskName, taskDescription, taskDate, taskPriority, taskProject);
     // function to display tasks on screen
     displayTask();
-} )
+  })
 
-taskList.addEventListener('click', (e) => {
-  if(e.target.classList.contains('round-mark')){
+ taskList.addEventListener('click', (e) => {
+   if(e.target.classList.contains('round-mark')){
       const task = allTask[Array.from(e.target.parentNode.parentNode.parentNode.children).indexOf(e.target.parentNode.parentNode)];
       task.completeTask();
       displayTask();
-  }
-})
+   }
+  })
 
   function addTaskToAllTasks(name, description, date, priority, project) {
     const newTask = new Task(name, description, date, priority, project);
     allTask.push(newTask);
   }
 
+  addTaskToAllTasks('CleanUp', 'Deep clean bedroom, living room, bathroom, and kitchen.', '5 Oct 2024', 'low', 'Chores');
+  addTaskToAllTasks('Finish Home Page', 'Fix bugs', '4 Oct 2024', 'Urgent', 'Website');
+  addTaskToAllTasks('Plan itinerary', 'Search for beautifull places.', '8 Oct 2024', 'Important', 'Japan Trip');
+
   function displayTask() {
     taskList.innerHTML = '';
     allTask.forEach(task => {
 
-        const taskDiv = document.createElement('div');
-        taskDiv.className = "task";
+      const taskDiv = document.createElement('div');
+      taskDiv.className = "task";
 
-        const taskContent = document.createElement('div');
-        taskContent.className = "task-content";
+      const taskContent = document.createElement('div');
+      taskContent.className = "task-content";
 
-        const completeTask = document.createElement('span');
-        completeTask.className = 'round-mark';
-        taskContent.appendChild(completeTask);
+      const completeTask = document.createElement('span');
+      completeTask.className = 'round-mark';
+      taskContent.appendChild(completeTask);
 
-        const taskTitle = document.createElement('p');
-        taskTitle.textContent = task.name;
-        taskTitle.className = 'title';
-        taskContent.appendChild(taskTitle);
+      const taskTitle = document.createElement('p');
+      taskTitle.textContent = task.name;
+      taskTitle.className = 'title';
+      taskContent.appendChild(taskTitle);
 
-        const taskDecription = document.createElement('p');
-        taskDecription.textContent = task.description;
-        taskDecription.className = 'description';
-        taskContent.appendChild(taskDecription);
+      const taskDecription = document.createElement('p');
+      taskDecription.textContent = task.description;
+      taskDecription.className = 'description';
+      taskContent.appendChild(taskDecription);
 
-        taskDiv.appendChild(taskContent);
+      taskDiv.appendChild(taskContent);
 
-        const taskDetails = document.createElement('div');
-        taskDetails.className = 'task-details';
+      const taskDetails = document.createElement('div');
+      taskDetails.className = 'task-details';
+      const taskPriority = document.createElement('p');
+      taskPriority.className = 'priority';
+      taskPriority.textContent = task.priority;
 
-        const taskPriority = document.createElement('p');
-        taskPriority.classList.add('priority')
-        taskPriority.textContent = task.priority;
+      if(task.priority == 'low') {
+          taskPriority.classList.add('low');
+       } else if(task.priority == 'Important') {
+          taskPriority.classList.add('important')
+       } else if(task.priority == 'Urgent') {
+          taskPriority.classList.add( 'urgent');
+       } 
+      taskDetails.appendChild(taskPriority);
 
-        if(task.priority == 'low') {
-            taskPriority.classList.add('priority', 'low');
-        } else if(task.priority == 'Important') {
-            taskPriority.classList.add('priority', 'important')
-        } else if(task.priority == 'Urgent') {
-            taskPriority.classList.add('priority', 'urgent');
-        } 
-        taskDetails.appendChild(taskPriority);
+      const taskDate = document.createElement('p')
+      taskDate.className = 'date';
+      taskDate.textContent = task.date;
+      taskDetails.appendChild(taskDate);
 
-        const taskDate = document.createElement('p')
-        taskDate.className = 'date';
-        taskDate.textContent = task.date;
-        taskDetails.appendChild(taskDate);
+      const taskProject = document.createElement('p');
+      taskProject.className = 'project-title';
+      taskProject.textContent = `#${task.project}`;
+      taskDetails.appendChild(taskProject);
 
-        const taskProject = document.createElement('p');
-        taskProject.className = 'project-title';
-        taskProject.textContent = `#${task.project}`;
-        taskDetails.appendChild(taskProject);
-
-        taskDiv.appendChild(taskDetails);
-
-        taskList.append(taskDiv)
+      taskDiv.appendChild(taskDetails);
+      taskList.appendChild(taskDiv)
     })
-}
+  }
+  displayTask()
 
+  // project stuffs
+  class Project {
+    constructor(name) {
+    this.name = name;
+    }
+    deleteProject() {
+    allProject.splice(allProject.indexOf(this), 1);
+    }
+  }
+
+  let allProject = []; 
+  const projectDialog = document.querySelector('.Project-dialog');
+  const projectList = document.querySelector('.project-list');
+
+  projectDialog.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const projectName = document.getElementById('project-Name').value
+    addProjectToAllProject(projectName);
+    displayProject();
+    displayOptions();
+  })
+
+  function addProjectToAllProject(name) {
+    const newproject = new Project(name);
+    allProject.push(newproject);
+  }
+
+  // default values
+  addProjectToAllProject('Chores')
+  addProjectToAllProject('Mental health');
+  addProjectToAllProject('Japan Trip')
+
+ function displayProject() {
+  projectList.innerHTML = '';
+
+  allProject.forEach(project => {
+
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'project';
+    projectDiv.textContent = project.name;
+
+    const projectIcon = document.createElement('div');
+    projectIcon.className = 'project-icon';
+
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = deleteicon;
+    deleteIcon.title = 'Delete';
+    deleteIcon.className = 'delete'
+    projectIcon.appendChild(deleteIcon);
+
+    projectDiv.appendChild(projectIcon);
+    projectList.appendChild(projectDiv);
+  })
+ }
+ displayProject()
+
+  projectList.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete')){
+      const project = allProject[Array.from(e.target.parentNode.parentNode.parentNode.children).indexOf(e.target.parentNode.parentNode)];
+      project.deleteProject();
+      displayProject();
+    }
+  })
+
+  // Adding options when project is added
+  const taskProjects = document.querySelector('#task-projects');
+
+  function displayOptions() {
+    taskProjects.innerHTML = '';
+
+    const defaultOption = document.createElement('option');
+    defaultOption.textContent = 'Tasks';
+    defaultOption.setAttribute('selected', true);
+    taskProjects.appendChild(defaultOption);
+
+    allProject.forEach(project => {
+      const option = document.createElement('option');
+      option.textContent = project.name;
+      taskProjects.appendChild(option)
+    })
+  }
+  displayOptions();
